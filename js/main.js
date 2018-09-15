fae09a0d66fc6ab4461c00bc06c07f44a5fd275a
Chris Tack <chris_tack@mac.com>
2018-09-15 00:17:35 -0700

    created html, js, css & added "x" and "o" images

---
+++ js/main.js
@@ -0,0 +1,40 @@
+/*----- constants -----*/
+      
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
+
+
+
+
+// Initialize Game
+      // Clear game board
+      // Click alternates between "x" or "o"
+
+// Player Won Game
+
+
+// Player Lost Game
+
+
+// Player Tied Game
+
+
+// Begin "New Game"
+
+
+// Reset Game
+
+
+// Randomly Select place to play
+
+// Check for gameBoard already occupied
